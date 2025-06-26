a4bc3f30b1dc125929073a150c6164c0e4428f7d
krnstha23 <krnstha23@gmail.com>
2025-06-26 16:58:31 +0545

    theme toggle added

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -3,7 +3,11 @@
     import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
     import { Separator } from '$lib/components/ui/separator/index.js';
     import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+    import SunIcon from '@lucide/svelte/icons/sun';
+    import MoonIcon from '@lucide/svelte/icons/moon';
 
+    import { toggleMode } from 'mode-watcher';
+    import { Button } from '$lib/components/ui/button/index.js';
     import '../../app.css';
 
     let { children } = $props();
@@ -14,6 +18,15 @@
     <Sidebar.Inset>
         <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
             <Sidebar.Trigger class="-ml-1" />
+            <Button onclick={toggleMode} variant="outline" size="icon">
+                <SunIcon
+                    class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+                />
+                <MoonIcon
+                    class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+                />
+                <span class="sr-only">Toggle theme</span>
+            </Button>
             <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
             <Breadcrumb.Root>
                 <Breadcrumb.List>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
     let { children } = $props();
     import { Toaster } from 'svelte-french-toast';
+    import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
 {@render children()}
 <Toaster />
